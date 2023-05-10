47b59ac099df55259726a03f75780a9ebe270963
gabyfle <gaby.santamaria@outlook.fr>
2023-05-10 20:12:19 +0200

    .

--- src/App.cpp
+++ src/App.cpp
@@ -1,10 +1,12 @@
 #ifdef __APPLE__
 #include "Objective-C/CocoaBridge.h"
 #endif
-#include "View/MainWindow/MainWindow.h"
+#include "Controller/GameCreationController.h"
+// #include "View/MainWindow/MainWindow.h"
 #include <QApplication>
 #include <QCoreApplication>
 #include <QMenuBar>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
@@ -15,13 +17,18 @@ int main(int argc, char *argv[]) {
   // and show it on the window
   // QCoreApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
 
+  auto ctrl = Controller::GameCreationController();
+  GameOptions * options = ctrl.createGameOption(1, ModeOptions::normal, false, false, 0);
+
+  std::cout << options->isExpert << std::endl;
+
 #ifdef __APPLE__
   // Objective-C code to disable the dictation and character
   // palette menu items from the Edit menu
   CocoaBridge::setDisabledDictationAndCharacterPaletteMenuItems();
 #endif
-  auto *mainWindow = new MainWindow();
-  mainWindow->show();
+  // auto *mainWindow = new MainWindow();
+  // mainWindow->show();
 
   return QApplication::exec();
 }
