4686c76a8ff24376f0aeba9f5777fa38df7d52e4
gabyfle <gaby.santamaria@outlook.fr>
2023-05-18 22:50:20 +0200

    fixed broken plantuml

--- src/Model/Shotten/Board/Entities/Border.cpp
+++ src/Model/Shotten/Board/Entities/Border.cpp
@@ -1,5 +1,7 @@
-//
-// Created by Pierre Roussel on 4/17/23.
-//
+/**
+* @author Gabriel Santamaria <gaby.santamaria@outlook.fr>
+* @date 5/10/2023
+* @copyright Copyright 2023 All rights reserved.
+*/
 
 #include "Border.h"
