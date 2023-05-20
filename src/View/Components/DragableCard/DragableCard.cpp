ced4c1c27172abe41fc765c42c5368f6acec7ece
gabyfle <gaby.santamaria@outlook.fr>
2023-05-20 22:58:20 +0200

    DragableCard
    - Created the DragableCard.cpp component, to be used to represent cards around.
    
    NOTE: this component is still in development and will need some tweaks in the future. Maybe we should opt for a Drag'n'Drop approach if it's too complicated to detect where the cards needs to be put.

---
+++ src/View/Components/DragableCard/DragableCard.cpp
@@ -0,0 +1,66 @@
+/**
+ * @author Gabriel Santamaria <gaby.santamaria@outlook.fr>
+ * @date 5/19/2023
+ * @copyright Copyright 2023 All rights reserved.
+ */
+
+#include "DragableCard.h"
+#include "../../Style/Style.h"
+#include "ui_DragableCard.h"
+
+DragableCard::DragableCard(QWidget *parent) : QLabel(parent), ui(new Ui::DragableCard) {
+  ui->setupUi(this);
+}
+
+void DragableCard::mousePressEvent(QMouseEvent* event) {
+  if (event->button() == Qt::LeftButton)
+    m_dragStartPosition = event->pos();
+}
+
+void DragableCard::mouseMoveEvent(QMouseEvent* event) {
+  if (!(event->buttons() & Qt::LeftButton))
+    return;
+  if ((event->pos() - m_dragStartPosition).manhattanLength()
+      < QApplication::startDragDistance())
+    return;
+
+  QPoint currentPosition = this->pos();
+
+  QPoint diff = event->pos() - m_dragStartPosition;
+
+  this->move(currentPosition + diff);
+}
+
+/**
+ * Sets the new image to render onto the screen
+ * @param imagePath
+ */
+void DragableCard::setImage(const QString& imagePath) {
+  this->backImage_ = QImage(imagePath);
+}
+
+void DragableCard::paintEvent(QPaintEvent *event) {
+  QPainter painter(this);
+  painter.setRenderHint(QPainter::Antialiasing);
+
+  if (!isEnabled()) {
+    painter.setPen(Style::dark());
+    painter.setOpacity(0.5);
+  } else if (underMouse() || hasFocus()) {
+    painter.setPen(Style::lightBlue());
+    painter.setOpacity(0.8);
+  } else {
+    painter.setPen(Style::dark());
+  }
+
+  painter.setBrush(Style::backgroundColorCard());
+
+  QPixmap pixmap = QPixmap::fromImage(backImage_);
+  QPixmap scaledPixmap = pixmap.scaled(this->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+  painter.drawPixmap(0, 0, scaledPixmap);
+}
+
+DragableCard::~DragableCard() {
+  delete ui;
+}
---
+++ src/View/Components/DragableCard/DragableCard.h
@@ -0,0 +1,46 @@
+
+/**
+ * @author Gabriel Santamaria <gaby.santamaria@outlook.fr>
+ * @date 5/19/2023
+ * @copyright Copyright } 2023 All rights reserved.
+ */
+
+#ifndef LO21_PROJECT_DRAGABLECARD_H
+#define LO21_PROJECT_DRAGABLECARD_H
+
+#include <QLabel>
+#include <QDrag>
+#include <QMimeData>
+#include <QMouseEvent>
+#include <QApplication>
+#include <QPainter>
+#include <QPixmap>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class DragableCard;
+}
+QT_END_NAMESPACE
+
+class DragableCard : public QLabel {
+  Q_OBJECT
+
+public:
+  explicit DragableCard(QWidget *parent = nullptr);
+  void setImage(const QString& imagePath);
+  ~DragableCard() override;
+
+protected:
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void paintEvent(QPaintEvent *event) override;
+
+private:
+  QPoint m_dragStartPosition;
+  Ui::DragableCard *ui;
+
+  /* This is the image that is painted to the screen */
+  QImage backImage_ = QImage("../assets/images/cards/card_test.png");
+};
+
+#endif//LO21_PROJECT_DRAGABLECARD_H
